0456837c85d5854153c327e6b7496ba3253e4f22
DevSeoRex <ch4570@naver.com>
2022-10-28 20:57:15 +0900

    자바의 정석 3판 복습 시작 - Day 1

---
+++ java3rd.ch14/lambda/LambdaEx2.java
@@ -0,0 +1,23 @@
+package java3rd.ch14.lambda;
+
+@FunctionalInterface
+interface MyFunction1 {
+    void myMethod(); // public abstract void myMethod();
+}
+
+public class LambdaEx2 {
+    public static void main(String[] args) {
+        MyFunction1 f = () -> {}; // MyFunction f = (MyFunction) ( () -> {} );
+        Object obj = (MyFunction1) ( () -> {}); // Object 타입으로 형변환이 생략됨
+        String str = ((Object)(MyFunction1) (() -> {})).toString();
+
+        System.out.println(f);
+        System.out.println(obj);
+        System.out.println(str);
+
+//        System.out.println(() -> {}); // 에러. 람다식은 Object 타입으로 형변환 안됨
+        System.out.println((MyFunction1) () -> {});
+//        System.out.println((MyFunction1) () -> {}).toSTring()); // 에러
+        System.out.println(((Object)(MyFunction1)(() -> {})).toString());
+    }
+}
